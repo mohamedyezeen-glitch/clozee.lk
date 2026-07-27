e30e1c47bd6a0b75408190c82accd64cf806991e
unknown <mohamedyezeen@gmail.com>
2026-07-27 16:15:05 +0530

    Updated CLOZEE website design

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,66 @@
 import "./App.css";
 
 function App() {
+
   return (
-    <div className="home">
+    <div>
 
       <nav>
         <h1>CLOZEE.LK</h1>
+
         <div>
           <span>Men</span>
           <span>Women</span>
           <span>New Arrivals</span>
-          <span>Collections</span>
         </div>
       </nav>
 
+
       <section className="banner">
-        <div className="content">
+
+        <div>
           <h2>CLOZEE</h2>
-          <p>Premium Fashion For The Modern Generation</p>
-          <button>Shop Now</button>
+
+          <p>
+            Premium Fashion For The Modern Generation
+          </p>
+
+          <button>
+            Shop Now
+          </button>
+
         </div>
+
       </section>
 
-      <section className="collection">
+
+      <section className="products">
+
         <h2>Latest Collection</h2>
 
-        <div className="products">
+        <div className="product-grid">
 
           <div className="card">
-            <h3>Oversized T-Shirts</h3>
-            <p>Comfort meets style</p>
+            <h3>Oversized Collection</h3>
+            <p>Premium Streetwear</p>
           </div>
 
-          <div className="card">
-            <h3>Street Wear</h3>
-            <p>Designed for confidence</p>
-          </div>
 
           <div className="card">
-            <h3>Premium Drops</h3>
-            <p>Limited fashion pieces</p>
+            <h3>New Arrivals</h3>
+            <p>Fresh Styles Coming Soon</p>
+          </div>
+
+
+          <div className="card">
+            <h3>Signature Drops</h3>
+            <p>Designed By CLOZEE</p>
           </div>
 
         </div>
 
       </section>
+
 
     </div>
   );
